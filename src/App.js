46408457b04e1e3670e016fc9e60d71e6be37250
Kirk Lefevre <kirklefevre@Kirks-MacBook-Air.local>
2024-10-05 16:40:45 -0400

    move frontend components to folder

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import './App.css';
-import Navbar from './Navbar';
+import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Modules from './Modules';
-import Module1 from './Module1';
-import Module2 from './Module2';
-import Module3 from './Module3';
-import Module4 from './Module4';
-import Module5 from './Module5';
-import Support from './Support'; // Import the Support component
-import Discussion from './Discussion';
-import FAQ from "./FAQ";
+import Home from './components/Home';
+import Modules from './components/Modules';
+import Module1 from './components/Module1';
+import Module2 from './components/Module2';
+import Module3 from './components/Module3';
+import Module4 from './components/Module4';
+import Module5 from './components/Module5';
+import Support from './components/Support'; // Import the Support component
+import Discussion from './components/Discussion';
+import FAQ from "./components/FAQ";
 
 function App() {
   return (
