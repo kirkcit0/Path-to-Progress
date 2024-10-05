62092ce27743e71cc4c922cc6b23a6b87b3c9df0
MiamiVibes <119141163+MiamiVibes@users.noreply.github.com>
2024-10-05 02:03:21 -0400

    new pages
    web pages for nav bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import React from 'react';
 import './App.css';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Modules from './Modules';
+import Support from './Support';
+import Discussion from './Discussion';
+import FAQ from './FAQ';
+import Login from './Login';
+import Home from './Home';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <header className="App-header">
-        <h1>Welcome, to your new journey...</h1>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/modules" element={<Modules />} />
+          <Route path="/support" element={<Support />} />
+          <Route path="/discussion" element={<Discussion />} />
+          <Route path="/faq" element={<FAQ />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
     <nav className="navbar">
       <ul className="navbar-links">
-        <li><a href="/">Home</a></li>
-        <li><a href="/support">Support</a></li>
-        <li><a href="/discussion">Discussion</a></li>
-        <li><a href="/faq">FAQ</a></li>
-        <li><a href="/login">Login/Signup</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/modules">Modules</Link></li>
+        <li><Link to="/support">Support</Link></li>
+        <li><Link to="/discussion">Discussion</Link></li>
+        <li><Link to="/faq">FAQ</Link></li>
+        <li><Link to="/login">Login/Signup</Link></li>
       </ul>
     </nav>
   );
