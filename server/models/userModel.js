57e092544f1b5193553da0d8db825f6beba57122
Kirk Lefevre <kirklefevre@Kirks-MacBook-Air.local>
2024-10-05 01:34:31 -0400

    update auth layout

---
+++ server/models/userModel.js
@@ -0,0 +1,34 @@
+// user model
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const passwordComplexity = require('joi-password-complexity');
+
+const userSchema = new mongoose.Schema({
+	name: {type: String, required: true, unique: true},
+	email: {type: String, required: true, unique: true},
+	password: {type: String, required: true},
+	// verified: {type: Boolean, default: false},
+
+    days: {type: Number, default: 1},
+    xp: {type: Number, default: 0},
+    level: {type: Number, default: 1},
+});
+
+userSchema.methods.generateAuthToken = function() {
+    const token = jwt.sign({id: this._id}, process.env.JWTPRIVATEKEY, {expiresIn: "1d"});
+    return token;
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+    const schema = Joi.object({
+        name: Joi.string().required().label("Username"),
+        email: Joi.string().email().required().label("Email"),
+        password: passwordComplexity().required().label("Password"),
+    });
+    return schema.validate(data)
+}
+
+module.exports = {User, validate};
